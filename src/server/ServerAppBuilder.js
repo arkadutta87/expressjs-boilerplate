497115e90bea4b3fd74ecbb7d183931cd8ab222f
arkadutta87 <arka.dutta@binaryworkers.com>
2017-01-03 15:19:15 +0530

    Login functionalty implemented

--- src/server/ServerAppBuilder.js
+++ src/server/ServerAppBuilder.js
@@ -6,6 +6,13 @@ import compression from 'compression';
 import bodyParser from 'body-parser';
 import ms from 'ms';
 import ExpressWinston from 'express-winston';
+import expressSession from 'express-session';
+import passwordless from 'passwordless';
+import MongoStore from 'passwordless-mongostore';
+import emailJs from 'emailjs';
+//import mongo from 'mongodb';
+import monk from 'monk';
+
 import buildLogger from './Logger';
 import apiRequestHandler from './ApiRequestHandler';
 
@@ -22,6 +29,52 @@ const NinetyDays = ms('90 days');
 //          services - service instances, that also implement registry methods
 //
 export default function (app, config) {
+    // TODO; enabled only when passwordless
+    // TODO: email setup (has to be changed)
+    const yourEmail = 'arkadutta0504@gmail.com';
+    const yourPwd = 'ise2009006shalmoli';
+    const yourSmtp = 'smtp.gmail.com';
+    const smtpServer = emailJs.server.connect({
+        user: yourEmail,
+        password: yourPwd,
+        host: yourSmtp,
+        ssl: true
+    });
+
+    const mongoHost = 'localhost';
+    const mongoPort = '27017';
+    const databaseMongo = 'passwordless-simple-mail';
+    const userDataBase = 'humane-cockpit-login-db';
+
+    // TODO: MongoDB setup (given default can be used)
+    const pathToMongoDb = `mongodb://${mongoHost}:${mongoPort}/${databaseMongo}`;
+
+    const db = monk(`${mongoHost}:${mongoPort}/${userDataBase}`);
+
+    // TODO: Path to be send via email -- to be configurable
+    const host = 'http://localhost/';
+
+    // Setup of Passwordless
+    passwordless.init(new MongoStore(pathToMongoDb));
+    passwordless.addDelivery(
+        (tokenToSend, uidToSend, recipient, callback) => {
+            console.log(`$$$$$$$$$$$--------- Token Send : ${tokenToSend} , uidToSend : ${uidToSend}`);
+            // Send out token
+            smtpServer.send({
+                text: `Hello!
+                You can now access your account here: ${host}?token=${tokenToSend}&uid=${encodeURIComponent(uidToSend)}`,
+                from: yourEmail,
+                to: recipient,
+                subject: `Token for ${host}`
+            }, (err, message) => {
+                if (err) {
+                    console.log(err);
+                }
+                callback(err);
+            });
+        });
+
+
     const logger = buildLogger(config.logDirectory || process.cwd());
 
     app.use(compression());
@@ -30,7 +83,7 @@ export default function (app, config) {
     ExpressWinston.requestWhitelist.push('params');
     ExpressWinston.requestWhitelist.push('cookies');
     ExpressWinston.requestWhitelist.push('body');
-    
+
     ExpressWinston.responseWhitelist.push('_headers');
 
     app.use(ExpressWinston.logger({winstonInstance: logger, statusLevels: true}));
@@ -50,11 +103,17 @@ export default function (app, config) {
         app.set('view engine', 'handlebars');
 
         if (config.client.publicPath) {
-            app.use(config.client.multiInstance ? '/:instanceName' : '/', express.static(config.client.publicPath, {index: false, maxAge: NinetyDays}));
+            app.use(config.client.multiInstance ? '/:instanceName' : '/', express.static(config.client.publicPath, {
+                index: false,
+                maxAge: NinetyDays
+            }));
         }
 
         if (config.client.resourcesPath) {
-            app.use(config.client.multiInstance ? '/:instanceName/resources' : '/resources', express.static(config.client.resourcesPath, {index: false, maxAge: NinetyDays}));
+            app.use(config.client.multiInstance ? '/:instanceName/resources' : '/resources', express.static(config.client.resourcesPath, {
+                index: false,
+                maxAge: NinetyDays
+            }));
         }
     }
 
@@ -71,6 +130,93 @@ export default function (app, config) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
+    app.use(expressSession({secret: '#4ghvdjs^&9BBJJsd', saveUninitialized: false, resave: false}));
+
+    //adding mongo handle to req
+    // Make our db accessible to our router
+    /*app.use((req, res, next) => {
+        req.db = db;
+        next();
+    });*/
+
+    // TODO; >>>>>> enabled only when passwordless
+    //passwordless code
+    app.use(passwordless.sessionSupport());
+    app.use(passwordless.acceptToken({successRedirect: '/'}));
+
+    // const router = express.Router();
+
+    /* GET login screen. */
+    app.get('/login', (req, res) => {
+        if (req.user) {
+            res.redirect('/');
+        } else if (req.loginFailed) {
+            res.render('login', {loginFailed: true});
+        } else {
+            res.render('login', {user: req.user});
+        }
+    });
+
+    /* POST login screen. */
+    app.post('/sendtoken',
+        passwordless.requestToken(
+            // Simply accept every user
+            (user, delivery, callback, req) => {
+                console.log(`User Object : ${JSON.stringify(user)}`);
+                console.log(`request object : ${JSON.stringify(req.body)}`);
+
+                //const dbInst = req.db;
+                const userName = req.body.user;
+                const instanceName = req.body.instanceName;
+
+                // Fetch from 'users' collection
+                const loginCollection = db.get('login');
+                loginCollection.findOne({username: userName, instanceName, isEnabled: true}, (e, doc) => {
+                    if (e) {
+                        console.log(e);
+                        req.loginStatus = false;
+                        callback(null, null);
+                    } else if (doc) {
+                        console.log(JSON.stringify(doc));
+                        //req.name = doc.name;
+                        callback(null, doc._id);
+                    } else {
+                        req.loginFailed = true;
+                        callback(null, null);
+                    }
+                });
+                // usually you would want something like:
+            }, {failureRedirect: '/login'}),
+        (req, res) => {
+            res.render('login', {tokenSent: true});
+        });
+
+    app.use(passwordless.restricted({failureRedirect: '/login'}));
+
+    app.use((req, res, next) => {
+        console.log('\n\n$$$$$$ ---- Inside The Code for Arka ----- $$$$$$$$ \n\n');
+        if (req.user) {
+            console.log('$$$$$ ---- Inside the code to set __userContext__ ');
+
+            //const dbInst = req.db;
+            const loginCollection = db.get('login');
+            loginCollection.findById(req.user, (error, user) => {
+                console.log(`User returned from ID --- : ${JSON.stringify(user)}`);
+                req.__userContext__ = user;
+                next();
+            });
+        } else {
+            next();
+        }
+    });
+
+    /* GET logout. */
+    app.get('/logout', passwordless.logout(),
+        (req, res) => {
+            res.redirect('/login');
+        });
+
+    // TODO; <<<<< enabled only when passwordless
 
     //
     // APIs
